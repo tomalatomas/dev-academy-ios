a6125aded17c8878d90936bd43d64de2c0863ea8
Tomáš Tomala <tomalatms@gmail.com>
2023-07-18 15:58:28 +0200

    feat: finish task-4

--- DevAcademy/Model/DataService.swift
+++ DevAcademy/Model/DataService.swift
@@ -13,8 +13,20 @@ import Foundation
 
 class DataService {
     static var shared: DataService = DataService()
+    var data: Result<Features, Error>?
     
     private init(){
+    }
+    
+    func fetchData(action: (Result<Features, Error>) -> Void) {
+        if let data {
+            action(data)
+            return()
+        }
+        
+        let timer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false) { [self] timer in
+            data = .success(DataService.mockData)
+        }
     }
 }
 
