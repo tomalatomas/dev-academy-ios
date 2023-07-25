4b6d28409a5953235360a306f0b064bb4b0fbe5a
Tomáš Tomala <tomalatms@gmail.com>
2023-07-25 16:32:09 +0200

    feat: move service, make fetch result nonoptional

---
+++ DevAcademy/Service/DataService.swift
@@ -0,0 +1,37 @@
+//
+// Filename: DataService.swift
+// Project: DevAcademy
+//
+// Developer:
+//  Name: Tomáš Tomala
+//  Email: tomalatms@gmail.com
+//
+// Description:
+//
+
+import Foundation
+
+class DataService {
+    static var shared: DataService = DataService()
+    var data: Result<Features, Error>?
+    
+    private init(){
+    }
+    
+    func fetchData(action: @escaping (Result<Features, Error>) -> Void) {
+        if let data {
+            action(data)
+            return
+        }
+        
+        _ = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false) { [weak self] timer in
+            let mockData = DataService.mockData
+            self?.data = .success(mockData)
+            action(self?.data ?? .success(mockData))
+        }
+    }
+}
+
+extension DataService {
+    private static var mockData: Features = Features(features: [])
+}
