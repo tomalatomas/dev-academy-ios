57d86213d801ccb19aab911c13f5ceba3f336864
Tomáš Tomala <tomalatms@gmail.com>
2023-08-08 01:24:09 +0200

    feat: remove unused tabbar

--- DevAcademy/ContentView.swift
+++ DevAcademy/ContentView.swift
@@ -2,18 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            PlacesView()
-                .tabItem {
-                    Label("Places", systemImage: "mappin.circle.fill")
-                }
-            
-            Color.red
-                .tabItem {
-                    Label("Next Tab", systemImage: "heart.fill")
-                }
-            
-        }
+        PlacesView()
     }
 }
 
